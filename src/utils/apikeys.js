d5f6b3427df03e07c4d73f0cda303c21c12c4673
JALAGANDESWER.R <jalagandeswer.r@gmail.com>
2021-08-23 13:11:50 +0530

    added apikry files to the commit

---
+++ src/utils/apikeys.js
@@ -0,0 +1,6 @@
+const weatherSatckApi="dda5f3378d7175dd114e1054930fd5c7",
+    geoCodingApi="pk.eyJ1IjoiamFsYXIiLCJhIjoiY2tzaG9kOW4zMDIzdzJwcjRjanEydHRnbiJ9._k67bmUQHjB5UN2oYoLemQ";
+module.exports={
+    weatherSatckApi,
+    geoCodingApi
+};
